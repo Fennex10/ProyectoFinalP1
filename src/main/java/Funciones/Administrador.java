e546f645bd02b1993e18d81c1bf4cdc24b43b997
pc <pc@DESKTOP-VI04E1C>
2024-11-12 21:20:27 -0400

    Administrador cambios

--- src/main/java/Funciones/Administrador.java
+++ src/main/java/Funciones/Administrador.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Funciones;
 
-/**
- *
- * @author pc
- */
 public class Administrador {
+  
+    public int IdAdministrador;
+    public String nombre;
+    public String apellido;
+    public String contraseña;
+    public String rol;
+
+    public int getIdAdministrador() {
+        return IdAdministrador;
+    }
+
+    public void setIdAdministrador(int IdAdministrador) {
+        this.IdAdministrador = IdAdministrador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Administrador(int IdAdministrador, String nombre, String apellido, String contraseña, String rol) {
+        this.IdAdministrador = IdAdministrador;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+    
     
 }
